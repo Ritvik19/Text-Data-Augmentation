fafed6f06a94515bfb513f571ef973a2d5d3b82d
Ritvik19 <rastogiritvik99@gmail.com>
2021-10-17 14:43:41 +0530

    Back Translation

--- text-data-augmentation/__init__.py
+++ text-data-augmentation/__init__.py
@@ -1,3 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from .easy_data_augmentation import *
+from .back_translation import *
---
+++ text-data-augmentation/back_translation.py
@@ -0,0 +1,23 @@
+from googletrans import Translator
+from tqdm.auto import tqdm
+
+
+class BackTranslation:
+    def __init__(self, base_language, interim_language, show_progress=True):
+        self.base_language = base_language
+        self.interim_language = interim_language
+        self.disable_progress = not show_progress
+        self.translator = Translator()
+
+    def __call__(self, x):
+        augmented = []
+        for doc in tqdm(x, disable=self.disable_progress):
+            augmented.append(doc)
+            try:
+                aug = self.translator.translate(text=doc, dest=self.interim_language).text
+                aug = self.translator.translate(text=aug, dest=self.base_language).text
+                augmented.append(aug)
+            except AttributeError:
+                pass
+
+        return augmented

4df0dc4b55463d427c6d52d7f860c722e97de6eb
Ritvik19 <rastogiritvik99@gmail.com>
2021-10-17 14:41:52 +0530

    Easy Data Augmentation

---
+++ text-data-augmentation/easy_data_augmentation.py
@@ -0,0 +1,81 @@
+import nltk
+import random
+from tqdm.auto import tqdm
+
+
+class EasyDataAugmentation:
+    def __init__(self, alpha=0.1, n_aug=4, operations=None, seed=None, show_progress=True):
+        self.alpha = alpha
+        self.operations = operations or ["insertion", "deletion", "swap", "shuffle"]
+        self.n_aug = n_aug
+        self.seed = seed
+        self.disable_progress = not show_progress
+        self.stopwords = nltk.corpus.stopwords.words("english")
+
+    def __get_synonym(self, word):
+        synonyms = set()
+        for syn in nltk.corpus.wordnet.synsets(word):
+            for l in syn.lemmas():
+                synonyms.add(l.name())
+        synonyms = [word] if len(synonyms) == 0 else list(synonyms)
+        return random.choice(synonyms)
+
+    def __insert(self, words):
+        r_idx_w = random.randint(0, len(words) - 1)
+        while words[r_idx_w] in self.stopwords:
+            r_idx_w = random.randint(0, len(words) - 1)
+        r_idx_i = random.randint(0, len(words) - 1)
+        syn = self.__get_synonym(words[r_idx_w])
+        words.insert(r_idx_i, syn)
+        return words
+
+    def __insertion(self, sentence):
+        words = nltk.word_tokenize(sentence)
+        for _ in range(int(self.alpha * len(words))):
+            words = self.__insert(words)
+        return " ".join(words)
+
+    def __deletion(self, sentence):
+        words = nltk.word_tokenize(sentence)
+        new_words = [word for word in words if random.random() > self.alpha]
+        return " ".join(new_words)
+
+    def __swap_words(self, words):
+        r_idx_1 = random.randint(0, len(words) - 1)
+        r_idx_2 = random.randint(0, len(words) - 1)
+        words[r_idx_1], words[r_idx_2] = words[r_idx_2], words[r_idx_1]
+        return words
+
+    def __swap(self, sentence):
+        words = nltk.word_tokenize(sentence)
+        for _ in range(int(self.alpha * len(words))):
+            words = self.__swap_words(words)
+        return " ".join(words)
+
+    def __shuffle(self, doc):
+        sentences = nltk.sent_tokenize(doc)
+        random.shuffle(sentences)
+        return " ".join(sentences)
+
+    def __call__(self, x):
+        random.seed(self.seed)
+        augmented = []
+        for sentence in tqdm(x, disable=self.disable_progress):
+            augmented.append(sentence)
+            for _ in range(self.n_aug):
+                operation = random.choice(self.operations)
+                if operation == "insertion":
+                    augmented.append(self.__insertion(sentence))
+                elif operation == "deletion":
+                    augmented.append(self.__deletion(sentence))
+                elif operation == "swap":
+                    augmented.append(self.__swap(sentence))
+                elif operation == "shuffle":
+                    augmented.append(self.__shuffle(sentence))
+                else:
+                    raise AttributeError(
+                        f"Invalid operation {operation}, valid operations are:"
+                        + "insertion, deletion, swap, shuffle."
+                    )
+        return augmented
+
